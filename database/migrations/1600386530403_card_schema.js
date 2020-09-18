3c31c491fae1466c0c64b013e3c0b89c88563438
infsolution <clsinfsolution@gmail.com>
2020-09-17 21:43:47 -0300

    add migration card

---
+++ database/migrations/1600386530403_card_schema.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CardSchema extends Schema {
+  up () {
+    this.create('cards', (table) => {
+      table.increments()
+      table.datetime('hour')
+      table.string('message')
+      table.float('value')
+      table.boolean('status')
+      table.timestamps()
+      table.integer('table_id')
+      table.foreign('table_id').references('tables.id').onDelete('cascade')
+    })
+  }
+
+  down () {
+    this.drop('cards')
+  }
+}
+
+module.exports = CardSchema
