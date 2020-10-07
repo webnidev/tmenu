01ada11bab3fbf696082711d36b426ee0641cb9f
infsolution <clsinfsolution@gmail.com>
2020-10-07 10:32:43 -0300

    implementing websocket server

--- app/Utils/Pdf.js
+++ app/Utils/Pdf.js
@@ -7,8 +7,6 @@ const fs = require('fs')
 
 class Pdf extends Model{
     async pdfCreate(printOrd){
-        //console.log(printOrd.code)
-        //console.log(printOrd.orders[0].product)
         const pageHeight = printOrd.orders.length * 10 + 150
         const pdfName = `establisment${printOrd.orders[0].establishment_id}card${printOrd.orders[0].card_id}orders${printOrd.orders[0].order_id}.pdf`
         const date = new Date()
@@ -61,53 +59,6 @@ class Pdf extends Model{
         return pdfName
     }
 
-    /*async createPdf({establishment,
-        table,
-        product,
-        order,
-        card,
-        auth}){
-        const pdfName = `establisment${establishment.id}card${card.id}order${order.id}.pdf`
-        const date = new Date()
-        const mounht = date.getMonth()+1
-        const day = `0${date.getDay()}`
-        const year = date.getUTCFullYear() 
-        const pdf = new PDFKit({size:[227.00, 350.50],
-    
-    layout: 'portrait',
-    margins : { 
-        top: 20, 
-        bottom: 20,
-        left: 20,
-        right: 20
-    }
-    })
-    let total = 0.0
-    pdf.fontSize(10)
-    pdf.text(establishment.name,20, 20, {align:'center'})
-    pdf.text(`CNPJ: ${establishment.cnpj}`,20,32, {align:'left'})
-    pdf.text(`${day}/${mounht}/${year}`, 80, 32, {align:'right'})
-    pdf.text(`${establishment.address}`, 20, 44,{align:'left'} )
-    pdf.text(`Cliente: ${auth.user.name}`, 120, 44, {align:'right'})
-    pdf.text(`--------------------------------------------------------`,20 , 52,{align:'left'})
-    pdf.text(`Garçom: ${card.waiter.name}`,20 , 62,{align:'left'})
-    pdf.text( `Mesa: ${table.number}`,80 , 62,{align:'right'})
-    pdf.text(`--------------------------------------------------------`,20 , 72,{align:'left'})
-    pdf.text(`Produto`,20, 82)
-    pdf.text(`Quantidade`,76, 82)
-    pdf.text(`Preço`,140, 82)
-    pdf.text(`Total`,180, 82)
-    pdf.text(`--------------------------------------------------------`,20 , 92,{align:'left'})
-    pdf.text(`${product.name}`, 20, 102)
-    pdf.text(`${order.quantity}`,96, 102)
-    pdf.text(` ${product.value}`, 130, 102)
-    pdf.text(`${order.quantity * product.value}`, 170, 102)
-    pdf.text(`--------------------------------------------------------`,20 , 112,{align:'left'})
-    pdf.text(`Total: ${total}`,120,122,{align: 'right'} )
-    pdf.pipe(fs.createWriteStream(`public/tmp/${pdfName}`))
-    pdf.end()
-    return pdfName
-    }*/
     createCardPdf({
         establishment,
         table,
@@ -115,13 +66,13 @@ class Pdf extends Model{
         auth,
         orders
     }){
-        const lengthPage = orders.length * 10 +150
+        const pageHeight = orders.length * 10 + 150
         const pdfName = `establisment${establishment.id}card${card.id}.pdf`
         const date = new Date()
         const mounht = date.getMonth()+1
         const day = `0${date.getUTCDate()}`.slice(-2)
         const year = date.getUTCFullYear()
-        const config = {size:[227.00, lengthPage],/*350.50 */
+        const config = {size:[227.00, pageHeight],
             layout: 'portrait',
             margins : { 
                 top: 20, 
@@ -149,8 +100,7 @@ class Pdf extends Model{
             pdf.text(`Total`,180, 82)
             pdf.text(`--------------------------------------------------------`,20 , 92,{align:'left'})
             let salt = 20
-            let i=0
-            //for(let j =0; j<orders.length; j+=20){   
+            let i=0 
                 for(i;i<orders.length;i++ ){
                     if(orders[i]){
                         pdf.text(`${i+1}`,20, position)
@@ -160,14 +110,7 @@ class Pdf extends Model{
                         pdf.text(parseFloat(`${orders[i].total}`).toFixed(2), 170, position)
                         position += 10
                     }
-                }
-                //salt += 20
-                // console.log(i)
-                // if(i<orders.length-1){
-                //     position = 20
-                //     pdf.addPage(config)
-                // }                
-            //}                   
+                }                 
             pdf.text(`--------------------------------------------------------`,20 , position,{align:'left'})
             pdf.text(`Total: ${parseFloat(card.value).toFixed(2) }`,120,position+10,{align: 'right'} )
             if(!fs.existsSync('public/tmp')){
