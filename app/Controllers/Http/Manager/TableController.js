4c23322ed589861a814da83c232dcce8fe6f1182
infsolution <clinfsolution@gmail.com>
2020-10-31 15:28:07 -0300

    add pagination

--- app/Controllers/Http/Manager/TableController.js
+++ app/Controllers/Http/Manager/TableController.js
@@ -2,6 +2,7 @@
 const Table = use('App/Models/Table')
 const Establishment = use('App/Models/Establishment')
 const Libs = use('App/Utils/Libs')
+const { validateAll } = use('Validator')
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
@@ -19,7 +20,7 @@ class TableController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, auth }) {
+  async index ({ request, response, auth, pagination }) {
     const establishment = await Establishment.query().where('user_id', auth.user.id).first()
     const tables = await Table.query()
     .where('establishment_id', establishment.id)
@@ -31,7 +32,7 @@ class TableController {
       .where('status','true')
       .orderBy('updated_at', 'desc')
     })
-    .fetch()
+    .paginate(pagination.page, pagination.limit)
     return response.send({tables})
   }
 
@@ -44,13 +45,27 @@ class TableController {
    * @param {Response} ctx.response
    */
   async store ({ request, response, auth }) {
+   try {
+     const errorMessage = {
+       'number.required':'O campo número é obrigatorio',
+       'number.number':'Este campo só aceita múneros inteiros'
+     } 
+    const validation = await validateAll(request.all(),{
+      number:'required|number'
+    },errorMessage)
+    if(validation.fails()){
+      return response.status(401).send({message: validation.messages()})
+    }
     const libs = new Libs
     const establishment = await Establishment.query().where('user_id', auth.user.id).first()
     const data = request.only(["number"])
     const table = await Table.create({...data, status: false, 
-      establishment_id: establishment.id,
-      hashcode:libs.hash()})
+    establishment_id: establishment.id,
+    hashcode:libs.hash()})
     return response.send({table})
+   } catch (error) {
+     return response.status(400).send({message: error.message})
+   }
   }
 
   /**
@@ -63,6 +78,16 @@ class TableController {
    * @param {View} ctx.view
    */
   async show ({ params, request, response, view }) {
+    try {
+      const table = await Table.findBy('id', params.id)
+      if(!table){
+        return response.status(404).send({message:'Mesa não encontrada!'})
+      }
+      await table.load('cards')
+      return response.send({table})
+    } catch (error) {
+      return response.status(500).send({message:error.message})
+    }
   }
 
   /**
@@ -74,16 +99,25 @@ class TableController {
    * @param {Response} ctx.response
    */
   async update ({ params, request, response, auth }) {
-    const data = request.all()
-    const establishment = await Establishment.query().where('user_id',auth.user.id).first()
-    if(!establishment){
-      return response.status(404).send({'response':'Table not found'})
+    try {
+      const errorMessage = {
+        'number.required':'O campo número é obrigatorio',
+        'number.number':'Este campo só aceita múneros inteiros'
+      } 
+      const validation = await validateAll(request.all(),{
+       number:'required|number'
+     },errorMessage)
+     if(validation.fails()){
+       return response.status(401).send({message: validation.messages()})
+     }
+      const data = request.all()
+      const table = await Table.query().findBy('id', params.id)
+      table.merge({...data})
+      await table.save()
+      return response.send({table})
+    } catch (error) {
+      return response.status(500).send({message:error.message})
     }
-    const table = await Table.query().where('establishment_id', establishment.id)
-    .where('id', params.id).first()
-    table.merge({...data})
-    await table.save()
-    return response.send({table})
 
   }
 
@@ -95,36 +129,17 @@ class TableController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response, auth }) {
-    const establishment = await Establishment.query().where('user_id',auth.user.id).first()
-    if(!establishment){
-      return response.status(404).send({'response':'Table not found'})
+  async destroy ({ params, request, response }) {
+    try {
+      const table = await Table.findBy('id',params.id)
+      if(!table){
+        return response.status(404).send({message:'Mesa não encontrada!'})
+      }
+      await table.delete()
+    return response.status(200).send({message:`A mesa ${table.number} foi excluida!`})
+    } catch (error) {
+      return response.status(500).send({message:error.message})
     }
-    const table = await Table.query().where('establishment_id', establishment.id)
-    .where('id', params.id).first()
-    await table.delete()
-    return response.status(204).send()
-  }
-
-   /**
-   * Display a single table.
-   * GET tables/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async menu ({ params, request, response }) {
-    const table = await Table.query().where('hashcode', params.slug).first()
-    if(!table){
-      return response.status(404).send({'response':'Cardápio não encontrado'})
-    }
-    const establishment = await table.establishment().first()//.categories().fetch()
-    const menu =await establishment.categories()
-    .with('products')
-    .fetch()
-    return response.send({menu})
   }
 }
 
