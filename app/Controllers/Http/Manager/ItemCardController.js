6f070f89646cab4ae2d8f624a486649b0c9aaf7a
infsolution <clinfsolution@gmail.com>
2021-03-22 18:39:14 -0300

    start method add order

--- app/Controllers/Http/Manager/ItemCardController.js
+++ app/Controllers/Http/Manager/ItemCardController.js
@@ -9,6 +9,10 @@ const Client = use('App/Models/Client')
 const Table = use('App/Models/Table')
 const ItemCard = use('App/Models/ItemCard')
 const Product = use('App/Models/Product')
+const OrderAttribute = use('App/Models/OrderAttribute')
+const OrderAttributeValue = use('App/Models/OrderAttributeValue')
+const Attribute = use('App/Models/Attribute')
+const OrderCard = use('App/Models/OrderCard')
 const Manager = use('App/Models/Manager')
 //const Printer = use('App/Models/Printer')
 const Database = use('Database')
@@ -59,7 +63,7 @@ class ItemCardController {
           
       } catch (error) {
         console.log(error)
-        return response.status(error.status).send(error.message)
+        return response.status(400).send({error:error.message})
       }
   }
 
@@ -73,7 +77,46 @@ class ItemCardController {
    */
   //Create order with multi itens
   async store ({ request, response, auth }) {
-    
+    const trx = await Database.beginTransaction()
+    try {
+      const data = request.all()
+      const table = await Table.find(data.table_id)
+      //const card = await Card.find(data.card_id)
+      const manager = await Manager.findBy('user_id', auth.user.id)
+      let client = await Client.query().where('user_id',data.user_id).where('company_id', company.id).first()
+      const company = await Company.find(manager.company_id)
+      const config = await company.configuration().first()
+      const printers = await company.printers().fetch()
+      const card = await Card.query().where('user_id', data.user.id).where('table_id',table.id).where('status', true).first()
+      let card_value = 0
+      if(!client){
+        const user = await User.find(data.user_id)
+        client = await Client.create({
+          name: user.name,
+          company_id:company.id, 
+          user_id: user.id}, trx)
+      }
+      if(!card){
+        card = await Card.create({
+          message:`${company.name} Cliente ${auth.user.name}`,
+          value: card_value,
+          table_id: table.id,
+          user_id: auth.user.id,
+          waiter_id:table.waiter_id,
+          owner:company.id,
+          printer_id: config.printer_card_id
+        },trx)
+      }
+      const orderCard = await OrderCard.create({table:table.number, value:0,status:'Em Andamento', company_id:company.id},trx)
+      let total_value_order = 0
+      await Promise.all(
+        //continua
+      )
+      console.log(data.itens)
+      return response.send({card})
+    } catch (error) {
+      return response.status(400).send({error:error.message})
+    }
   }
 
   /**
@@ -103,7 +146,7 @@ class ItemCardController {
       return response.send({item})
     } catch (error) {
       console.log(error)
-      return response.status(error.status).send(error.message)
+      return response.status(400).send({error:error.message})
     }
 
   }
@@ -153,7 +196,7 @@ class ItemCardController {
     return response.send(cards.rows[0])
     } catch (error) {
       console.log(error)
-      return response.status(error.status).send(error.message)
+      return response.status(400).send({error:error.message})
     }
   }
 }
